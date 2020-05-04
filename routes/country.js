6d85594f7b55e9178e78b39f6f3b750e2d178578
Louis Evans <louis_evans@icloud.com>
2020-05-04 14:33:17 +0100

    completed delete action and added post action for add

--- routes/country.js
+++ routes/country.js
@@ -2,9 +2,13 @@ const express = require('express');
 const sqlite3 = require('sqlite3');
 const router = express.Router();
 
+const getDatabase = (mode, callback) => {
+  return new sqlite3.Database('./db/spiritz.db', mode, callback);
+};
+
 router.get('/all', function(req, res, next) {
 
-  const db = new sqlite3.Database('./db/spiritz.db', sqlite3.OPEN_READONLY, (err) => {
+  const db = getDatabase(sqlite3.OPEN_READONLY, (err) => {
 
     if(err) 
     {
@@ -26,9 +30,42 @@ router.get('/all', function(req, res, next) {
   });
 });
 
+router.post('/add', function(req, res, next) 
+{
+  const db = getDatabase(sqlite3.OPEN_READWRITE, (err) => {
+
+    db.run(`INSERT INTO Country(CountryName) VALUES(?)`, [req.body.name], function (err)
+    {
+      if (err) 
+      {
+        res.sendStatus(400);
+        return;
+      }
+
+      res.json({
+        CountryId: this.lastID,
+        CountryName: req.body.name
+      });
+
+    });
+  });
+});
+
 router.delete('/', function(req, res, next){
-    console.log(req.query['id']);
-    res.send('OK');
+
+    const db = getDatabase(sqlite3.OPEN_READWRITE, (err) => {
+
+      db.run(`DELETE FROM Country WHERE CountryId = (?)`, [req.query['id']], function (err)
+      {
+        if (err) 
+        {
+          res.sendStatus(400);
+          return;
+        }
+  
+        res.send('OK');
+      });
+    });
 })
 
 module.exports = router;
