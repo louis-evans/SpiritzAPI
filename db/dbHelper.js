bc66177b7e98ef215c663b2b0d8b1493d5513ec8
Louis Evans <louis_evans@icloud.com>
2020-05-15 17:19:00 +0100

    getDatabase callback is now optional

--- db/dbHelper.js
+++ db/dbHelper.js
@@ -1,7 +1,11 @@
 const sqlite3 = require('sqlite3');
 
 module.exports = {
-    getDatabase(mode, callback) {
-        return new sqlite3.Database('./db/spiritz.db', mode, callback);
+    
+    getDatabase(mode, callback) 
+    {
+        if(callback) return new sqlite3.Database('./db/spiritz.db', mode, callback);
+        
+        return new sqlite3.Database('./db/spiritz.db', mode);
     }
 }
