4ec95455524102f96ea9aca7af0e84935ec41317
Louis Evans <50640262+louis-evans@users.noreply.github.com>
2020-07-08 13:59:34 +0100

    Update dbHelper.js

--- db/dbHelper.js
+++ db/dbHelper.js
@@ -1,11 +1,12 @@
 const sqlite3 = require('sqlite3');
+const dbPath = './db/spiritz.db';
 
 module.exports = {
     
     getDatabase(mode, callback) 
     {
-        if(callback) return new sqlite3.Database('./db/spiritz.db', mode, callback);
+        if(callback) return new sqlite3.Database(dbPath, mode, callback);
         
-        return new sqlite3.Database('./db/spiritz.db', mode);
+        return new sqlite3.Database(dbPath, mode);
     }
 }
